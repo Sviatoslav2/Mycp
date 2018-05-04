5e7a7a84d23d2f23d4192375dd050bb107a8d425
Sviatoslav2 <fedoriv@ucu.edu.ua>
2018-05-04 05:25:45 +0300

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/mycp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mycp.dir/main.cpp.obj"
+  "mycp.pdb"
+  "mycp.exe"
+  "mycp.exe.manifest"
+  "libmycp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mycp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
